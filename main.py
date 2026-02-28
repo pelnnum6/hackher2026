67fb26850b2f9e0b629eb85bd48f98b7dabe3c5c
pelnnum6 <pelinsumete209@gmail.com>
2026-02-28 12:32:57 -0500

    fast api active

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #main page 
+from pydantic import BaseModel
+
+class QuizAnswer(BaseModel):
+    category: str
+    shared: bool
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
